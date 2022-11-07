ccc9ca17089d62bff28af30e24a952319de02491
kazi <grafix147@gmail.com>
2022-11-07 16:05:53 +0600

    kazi

---
+++ src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { AddressComponent } from './address/address.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    HeaderComponent,
+    AddressComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatButtonModule,
+    FormsModule
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  userData = {
+    "ID":"",
+    "firstName":"",
+    "lastName":"",
+    "faName":"",
+    "faOccu":"",
+    "maName":"",
+    "maOccu":"",
+    "pHouse":"",
+    "proad":"",
+    "pPolice":"",
+    "pPost":"",
+    "pPostCode":"",
+    "pDistrict":"",
+    "pCountry":"",
+    "mNumber":"",
+    "pNumber":"",
+    "email":"",
+    "gender":"",
+    "religion":"",
+    "nationality":"",
+
+};
+
+ }
